32796affedb8e065a3a067cefdf2127ee51f2b7e
Usmon <noname4v7@gmail.com>
2024-01-18 17:34:15 +0300

    ToDo v2

--- src/app/app.js
+++ src/app/app.js
@@ -9,6 +9,26 @@ let delComplete = document.querySelector(".delComplete");
 
 let todos = [];
 
+if (window.localStorage.length > 0) {
+  todos = JSON.parse(window.localStorage.getItem("json"));
+}
+
+renderTaskNode();
+
+let checkArr = (arr) => {
+  if (arr.length > 0) {
+    body.append(line);
+    body.append(ul);
+    body.append(btns);
+  } else {
+    btns.remove();
+    ul.remove();
+    line.remove();
+  }
+};
+
+checkArr(todos);
+
 let addTask = () => {
   if (input.value.trim().length) {
     todos.push({
@@ -22,104 +42,96 @@ let addTask = () => {
 const toggleIsCompleted = (id) => {
   let task = todos.find((el) => el.id === id);
   task.isComleted = !task.isComleted;
+  addLocal();
 };
 
-let createTaskNode = (obj) => {
+function createTaskNode(obj) {
+  console.log(obj);
   const li = document.createElement("li");
   li.classList.add("ToDo");
   li.innerHTML = `
     <input type="checkbox" name="" id="input">
-    <p>${obj.text}</p>
-    <button>❌</button>
+    <p >${obj.text}</p>
+    <button class='redo'>✏️</button>
+    <button class='del'>❌</button>
     `;
   const check = li.querySelector("#input");
+  check.checked = obj.isComleted;
   check.addEventListener("change", () => toggleIsCompleted(obj.id));
-  return li;
-};
 
-let renderTaskNode = () => {
+  let edit = li.querySelector(".redo");
+  edit.addEventListener("click", () => editTask(li, obj));
+
+  let cros = li.querySelector(".del");
+  cros.addEventListener("click", () => {
+    li.remove();
+    todos = todos.filter((el) => el.id !== obj.id);
+    console.log(todos);
+    addLocal();
+    checkArr(todos);
+  });
+  return li;
+}
+
+function renderTaskNode() {
   ul.innerHTML = "";
   todos.forEach((el) => ul.append(createTaskNode(el)));
-};
-
-btns.remove();
-ul.remove();
-line.remove();
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   addTask();
   renderTaskNode();
+  addLocal();
+  checkArr(todos);
   input.value = "";
-  let btn = ul.querySelectorAll("button");
-  btn.forEach((el) =>
-    el.addEventListener("click", () => {
-      let index = todos.indexOf(el.parentElement.children[1].textContent);
-      todos.splice(index, 1);
-      console.log(todos);
-      el.parentElement.remove();
-      if (todos.length > 0) {
-        body.append(line);
-        body.append(ul);
-        body.append(btns);
-      } else {
-        btns.remove();
-        ul.remove();
-        line.remove();
-      }
-    })
-  );
-  if (todos.length > 0) {
-    body.append(line);
-    body.append(ul);
-    body.append(btns);
-  } else {
-    btns.remove();
-    ul.remove();
-    line.remove();
-  }
 });
+
+function editTask(el, obj) {
+  el.innerHTML = `
+     <input type="checkbox" name="" id="input">
+     <input class='redoText' type='text' value='${obj.text}' >
+     <button class='redov'>✅</button>
+     <button class='delv'>❌</button>
+     `;
+  let edit = el.querySelector(".redov");
+  let input = el.querySelector(".redoText");
+  edit.addEventListener("click", () => {
+    if (input.value == "") {
+      obj.text;
+    } else {
+      obj.text = input.value;
+    }
+    addLocal();
+    el.replaceWith(createTaskNode(obj));
+  });
+}
 
 delAll.addEventListener("click", () => {
   todos = [];
   ul.innerHTML = "";
-  console.log(todos);
-  if (todos.length > 0) {
-    body.append(line);
-    body.append(ul);
-    body.append(btns);
-  } else {
-    btns.remove();
-    ul.remove();
-    line.remove();
-  }
+  addLocal();
+  checkArr(todos);
 });
 
 delComplete.addEventListener("click", () => {
-  let checkbox = ul.querySelectorAll("input");
-  checkbox.forEach((el) => {
-    if (el.checked) {
-      let index = todos.indexOf(el.parentElement.children[1].textContent);
-      todos.splice(index, 1);
-      el.parentElement.remove();
-    }
-    if (todos.length > 0) {
-      body.append(line);
-      body.append(ul);
-      body.append(btns);
-    } else {
-      btns.remove();
-      ul.remove();
-      line.remove();
-    }
-  });
-  if (todos.length > 0) {
-    body.append(line);
-    body.append(ul);
-    body.append(btns);
-  } else {
-    btns.remove();
-    ul.remove();
-    line.remove();
-  }
+  todos = todos.filter((el) => !el.isComleted);
+  ul.innerHTML = "";
+  renderTaskNode();
+  addLocal();
+  checkArr(todos);
 });
+
+function trueFalse(num) {
+  // if (num == 1) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+  num == 1 ? true : false;
+}
+
+function addLocal() {
+  let json = JSON.stringify(todos);
+  window.localStorage.setItem("json", json);
+}
